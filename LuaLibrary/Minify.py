df45fb90043a4802b6abf38043c76cb3e73f33f5
StiviK <stefan.kuerzeder@gmail.com>
2017-03-13 18:19:23 +0100

    Added MINIFY_MODE to minify.py

--- LuaLibrary/Minify.py
+++ LuaLibrary/Minify.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import re
+import sys
 
 # List files here
+MINIFY_MODE = bool(int(sys.argv[1]))
 OUTPUT_PATH = 'MTATD.bundle.lua'
 FILES = [
     'MTATD.lua',
@@ -13,15 +15,19 @@ FILES = [
 ]
 
 # Create output file
+print("Minify mode is {status}".format(status="enabled" if MINIFY_MODE else "disabled"))
 with open(OUTPUT_PATH, 'w') as outputFile:
     # Write all files
     for path in FILES:
         with open(path) as file:
             for line in file:
-                if line.find('--') == -1:
-                    if not re.match(r'^\s*$', line):
-                        outputFile.write(line
-                            .replace('\n', ' ')
-                            .replace('[[', '')
-                            .replace(']]', '')
-                        )
+                if MINIFY_MODE:
+                    if line.find('--') == -1:
+                        if not re.match(r'^\s*$', line):
+                            outputFile.write(line
+                                .replace('\n', ' ')
+                                .replace('[[', '')
+                                .replace(']]', '')
+                            )
+                else:
+                    outputFile.write(line)

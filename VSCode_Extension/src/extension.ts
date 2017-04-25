d9714ebf8f481bd6a219d31cbb031508f4c58dbd
Jusonex <jusonex96@gmail.com>
2017-04-25 22:22:55 +0200

    Add VSCode command that starts an MTA server

--- VSCode_Extension/src/extension.ts
+++ VSCode_Extension/src/extension.ts
@@ -4,7 +4,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { spawn, ChildProcess } from 'child_process';
+import { exec, ChildProcess } from 'child_process';
+import { normalize } from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,15 +17,19 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.startMTA', () => {
         // The code you place here will be executed every time your command is executed
         const config = vscode.workspace.getConfiguration('launch');
-        const info = config.get('configurations');
+        let info = config.get<Array<any>>('configurations');
         if (info) {
-            // Display a message box to the user
-            vscode.window.showInformationMessage('TestServer started');
+            info = info.filter(v => v.type === "mtasa");
 
-            vscode.window.showInformationMessage(info[0].program);
-            spawn(info[0].program);
+            if (info[0]) {
+                // Display a message box to the user
+                vscode.window.showInformationMessage('Starting MTA:SA server right now');
+
+                // Start server
+                const path = normalize(info[0].serverpath + '/MTA Server_d.exe');
+                exec(`start cmd.exe /K "${path}"`); // TODO: Insert packaged path here
+            }
         }
-
     });
 
     context.subscriptions.push(disposable);
